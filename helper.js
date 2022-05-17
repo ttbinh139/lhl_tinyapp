aab0e0eb94cf215b34d6730bf10b12f66e500883
ttbinh139 <ttbinh139@gmail.com>
2022-05-17 04:37:29 +0000

    Implement create new short URL

---
+++ helper.js
@@ -0,0 +1,20 @@
+//const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateRandomString(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+
+
+
+
+console.log(generateRandomString(8));
+
+module.exports = { generateRandomString };
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const { response } = require("express");
-const express = require("express")
+const { generateRandomString } = require('./helper');
+
+const express = require("express");
 const app = express();
 const PORT = 8080;
-const urlDatabases = {
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let urlDatabases = {
   '1a2b3c' : "https://www.lighthouselabs.ca",
   '2b3c4d' : "https:///www.google.ca"
 }
@@ -16,11 +22,35 @@ app.get('/', (request, response) => {
   response.render("pages/index.ejs", urls);
 });
 
+app.get('/urls', (request, response) => {
+  const urls = {urls: urlDatabases};
+  response.render("pages/index.ejs", urls);
+});
+
 app.get("/urls/:shortURL", (request, response) => {
   const url = { shortURL: request.params.shortURL, longURL: urlDatabases[request.params.shortURL] };
   //console.log(url);
   response.render("pages/urls_show", url);
 });
+
+// Create new url
+app.get('/new', (request, response) => {
+  response.render("pages/new");
+});
+
+app.post("/new", (request, response) => {
+  const randomString = generateRandomString();
+  urlDatabases[randomString] = request.body.longURL;
+  response.redirect(`/urls/${randomString}`);
+  //console.log(request.body);  // Log the POST request body to the console
+  //response.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortUrl", (request, response) => {
+  const longUrl = urlDatabases[request.params.shortUrl];
+  //console.log(longUrl);
+  response.redirect(longUrl);
+})
 
 // About pages
 app.get('/about', (request, response) => {
