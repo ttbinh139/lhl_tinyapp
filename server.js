1eec055e78c64c707cd7b7e1312c2473868d63ec
ttbinh139 <ttbinh139@gmail.com>
2022-05-18 03:42:25 +0000

    Implement edit URL feature

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/urls', (request, response) => {
   response.render("pages/index.ejs", urls);
 });
 
+// View a URL
 app.get("/urls/:shortURL", (request, response) => {
   const url = { shortURL: request.params.shortURL, longURL: urlDatabases[request.params.shortURL] };
   response.render("pages/urls_show", url);
@@ -43,12 +44,24 @@ app.post("/new", (request, response) => {
   response.redirect(`/urls/${randomString}`);
 });
 
+// Update URL
+app.post("/update/:shortURL", (request, response) => {
+  //response.send(request.body.longURL);
+  const shortURL = request.params.shortURL;
+  const longURL = request.body.longURL;
+  urlDatabases[shortURL] = longURL;
+  //response.send(urlDatabases);
+  response.redirect("/");
+});
+
+// Redirec URL
 app.get("/u/:shortURL", (request, response) => {
   console.log(urlDatabases);
   const longUrl = urlDatabases[request.params.shortURL];
   response.redirect(longUrl);
 })
 
+// Delete URL
 app.get("/delete/:shortURL", (request, response) => {
   //response.send(request.params);
   const shortURL = request.params.shortURL;
